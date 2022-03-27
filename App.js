7bec43f1484645c8094520865c4882bd7b408b02
Shamsher <shamsher@geekyants.com>
2022-03-27 19:49:30 +0530

    icon added in navigation

--- App.js
+++ App.js
@@ -1,16 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { NativeBaseProvider } from 'native-base';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import Navigation from './navigation';
+import { NativeBaseProvider } from "native-base";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Navigation from "./navigation";
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <NativeBaseProvider>
-          <Navigation />
-        <StatusBar style="auto" />
+        <Navigation />
       </NativeBaseProvider>
     </GestureHandlerRootView>
   );
 }
-
--- navigation/index.js
+++ navigation/index.js
@@ -2,16 +2,40 @@ import { NavigationContainer } from "@react-navigation/native";
 import Home from "../screens/Home";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Account from "../screens/Account";
+import { Fontisto, FontAwesome5 } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 const Navigation = () => {
-    return (
-        <NavigationContainer>
-            <Tab.Navigator initialRouteName="Home">
-                <Tab.Screen name="Home" component={Home} options={{headerShown: false}}/>
-                <Tab.Screen name="Account" component={Account} options={{headerShown: false}}/>
-            </Tab.Navigator>
-        </NavigationContainer>
-    )
-}
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            if (route.name === "Home") {
+              return <Fontisto name="home" size={size} color={color} />;
+            } else if (route.name === "Account") {
+              return (
+                <FontAwesome5 name="user-circle" size={size} color={color} />
+              );
+            }
+          },
+          tabBarActiveTintColor: "#2D2332",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="Account"
+          component={Account}
+          options={{ headerShown: false }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
 export default Navigation;
