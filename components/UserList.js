fccadb4fed6ca2f80aff03cba6853205f2ade083
Shamsher <shamsher@geekyants.com>
2022-03-30 13:17:45 +0530

    animation added

--- components/UserList.js
+++ components/UserList.js
@@ -1,18 +1,43 @@
 import { FlatList, View } from "native-base"
 import UserInfo from "./UserInfo";
+import { Animated } from "react-native";
+import { useRef } from "react";
 
 const UserList = ({userList,setUserList,flatListRef,fetchdata}) => {
-    const renderItem = ({ item }) => (
-        <UserInfo user={item} removeUser={removeUser}/>
-    );
+    const ITEM_HEIGHT = 100;
+    const scrollY = useRef(new Animated.Value(0)).current;
 
+    const renderItem = ({ item, index }) => {
+        const inputRange = [
+            -1,
+            0,
+            ITEM_HEIGHT * index,
+            ITEM_HEIGHT * (index + 2)
+        ]
+
+        const scale = scrollY.interpolate({
+            inputRange,
+            outputRange: [1, 1, 1, 0],
+        })
+
+        return (
+            <Animated.View style={{ transform: [{ scale }], height:ITEM_HEIGHT }}>
+                <UserInfo user={item} removeUser={removeUser}/>
+            </Animated.View>
+        )
+        };
+    
     const removeUser = (user) => {
         setUserList(userList.filter(u => u.phone !== user.phone))
     }
 
     return (
         <View mt="12" >
-            <FlatList
+            <Animated.FlatList
+                onScroll={Animated.event(
+                    [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+                    { useNativeDriver: true }
+                )}
                 ref={flatListRef}
                 data={userList}
                 renderItem={renderItem}
